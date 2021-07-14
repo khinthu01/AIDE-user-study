337478f03faff766cd1ff48d6ce0a673cb8645ea
khinthu01 <khinthu01@gmail.com>
2021-07-14 14:29:41 +1000

    setting up routing module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+const routes = require('./routes');
 const app = express();
 
 const port = 3000;
@@ -9,10 +10,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static'))); // important if css/js/image files from static are required
-
-app.get('/', (request, response) => {
-  response.render('pages/index', { pageTitle: 'Welcome' });
-});
+app.use('/', routes());
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}!`);
