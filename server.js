ae0b487865143c9afacb4406e02f1e5258e5ab83
khinthu01 <khinthu01@gmail.com>
2021-07-14 13:55:24 +1000

    commit on server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const path = require('path');
 
-const app = express ();
+const app = express();
 
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, './static'))); // important if css/js/image files from static are required
 
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, '/static/index.html'));
+  response.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Express server listening on port ${port}!`);
+  console.log(`Express server listening on port ${port}!`);
 });
