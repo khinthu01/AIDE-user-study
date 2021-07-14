a16c77686adb9a172d0dcdfd31ce37a5cb1904ac
khinthu01 <khinthu01@gmail.com>
2021-07-14 14:04:26 +1000

    ejs setup

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, './views'));
 app.use(express.static(path.join(__dirname, './static'))); // important if css/js/image files from static are required
 
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, '/static/index.html'));
+  response.render('pages/index', { pageTitle: 'Welcome' });
 });
 
 app.listen(port, () => {
