187aa755673e4d9efdcfbc0e46c1ff3eb6d5b56a
khinthu01 <khinthu01@gmail.com>
2021-07-14 14:02:05 +1000

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './static'))); // important if css/js/image files from static are required
 
 app.get('/', (request, response) => {
