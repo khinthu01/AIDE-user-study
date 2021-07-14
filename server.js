4fdc3988051c3e0f72c461581b2d773b8b600def
khinthu01 <khinthu01@gmail.com>
2021-07-14 13:42:06 +1000

    commit on server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express ();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, './static'))); // important if css/js/image files from static are required
+
 app.get('/', (request, response) => {
-    response.send('Hello Express');
+    response.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
 app.listen(port, () => {
