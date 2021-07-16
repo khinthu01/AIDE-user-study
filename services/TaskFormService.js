350bff03a021b185c67e9cbef5c27667f01f4d3b
khinthu01 <khinthu01@gmail.com>
2021-07-16 16:46:37 +1000

    commit on taskformserver

--- services/TaskFormService.js
+++ services/TaskFormService.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const util = require('util');
 
 const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 class TaskFormService {
   constructor(datafile) {
