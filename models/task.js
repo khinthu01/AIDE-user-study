476a4713bc481002f67540823321ea9a157dea78
khinthu01 <khinthu01@gmail.com>
2021-07-16 18:09:02 +1000

    setting up model

---
+++ models/task.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+  task_id: Number,
+  task_title: String,
+  text: String,
+  prompted: { type: Boolean, default: false },
+});
+
+const Task = mongoose.model('task', taskSchema);
