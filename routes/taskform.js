5cb145cd39187ad7b365e6ccc13018eb9f57702a
khinthu01 <khinthu01@gmail.com>
2021-07-16 15:33:28 +1000

    changes to taskform

--- routes/taskform.js
+++ routes/taskform.js
@@ -6,18 +6,24 @@ const router = express.Router();
 module.exports = (params) => {
   const { taskFormService } = params;
 
-  router.get('/:task_id', async (request, response) => {
-    const title = `Task ${request.params.task_id}`;
-    const taskform = await taskFormService.getQuestions(request.params.task_id);
-    console.log(taskform);
-    response.render('layout/layout', {
-      pageTitle: title,
-      template: 'taskform',
-      taskform,
-    });
+  router.get('/:task_id', async (request, response, next) => {
+    try {
+      const title = `Task ${request.params.task_id}`;
+      const taskform = await taskFormService.getQuestions(request.params.task_id);
+      // console.log(taskform);
+      response.render('layout/layout', {
+        pageTitle: title,
+        template: 'taskform',
+        taskform,
+      });
+    } catch (err) {
+      return next(err);
+    }
   });
 
-  router.post('/', (request, response) => {});
+  router.post('/', (request, response) => {
+    response.send('Task completed');
+  });
 
   return router;
 };
