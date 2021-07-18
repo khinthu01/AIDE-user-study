ae8a8fe21389c837ee62f35b02c2399f842c09ab
khinthu01 <khinthu01@gmail.com>
2021-07-18 14:01:42 +1000

    response and taskform models

--- models/taskform.js
+++ models/taskform.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const taskFormSchema = new mongoose.Schema({
-  _id: { type: mongoose.Schema.Types.ObjectId, ref: 'tasks' },
+  _task: { type: mongoose.Schema.Types.ObjectId, ref: 'tasks' },
   task_title: { type: String },
   participant_id: { type: String },
   q1: { type: String },
