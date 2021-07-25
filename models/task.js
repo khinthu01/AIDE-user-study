2c0bb9d59c54058722234ca764f61db183a1a710
khinthu01 <khinthu01@gmail.com>
2021-07-25 14:29:20 +1000

    added audio for task 2

--- models/task.js
+++ models/task.js
@@ -4,6 +4,7 @@ const taskSchema = new mongoose.Schema({
   task_title: { type: String },
   audio_title: { type: String },
   audio_Url: { type: String },
+  prompt_time: { type: Number },
   text: { type: String },
   prompted: { type: Boolean, default: false },
 });
